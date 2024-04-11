41c141aae2239e32df65f60517783f2b2dad73ad
Fran <kpsuan@up.edu.ph>
2024-04-12 00:40:51 +0800

    create announement functionality

--- client/src/Dashboard/Components/Admin/Announcements/CreatePost.jsx
+++ client/src/Dashboard/Components/Admin/Announcements/CreatePost.jsx
@@ -18,6 +18,7 @@ import { Link } from 'react-router-dom';
 import { useState } from "react";
 import {CircularProgressbar} from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import { useNavigate } from "react-router-dom";
 
 
 const CreatePost = () => {
@@ -25,6 +26,8 @@ const CreatePost = () => {
     const [imageUploadProgress, setImageUploadProgress] = useState(null);
     const [imageUploadError, setImageUploadError] = useState(null);
     const [formData, setFormData] = useState({});
+    const [publishError, setPublishError] = useState(null);
+    const navigate = useNavigate();
 
     const handleUploadImage = async () => {
         try {
@@ -62,7 +65,34 @@ const CreatePost = () => {
             setImageUploadProgress(null);
             console.log(error);
     }
-}
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch('/api/post/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+            if(!res.ok){
+                setPublishError(data.message);
+                return
+            }
+      
+            if(res.ok){
+                setPublishError(null);
+                navigate(`/announcement/${data.slug}`);
+            }
+        }
+        catch (error) {
+            setPublishError('Something went wrong.');
+        }
+    };
+
     return (
         <div className="dashboard my-flex">
           <div className="dashboardContainer my-flex">
@@ -71,12 +101,13 @@ const CreatePost = () => {
             <Top />
             <div className="p-3 max-w-3xl mx-auto min-h-screen"> 
                 <h1 className="text-center text-3xl my-7 font-semibold">Create Announcement</h1>
-                <form className="flex flex-col gap-4">
+                <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
                     <div className="flex flex-col gap-4 sm:flex-row justify-between">
                         <TextInput type='text' placeholder='Title' required id ='title' 
-                        className = 'flex-1' />
+                        className = 'flex-1' onChange={(e) => setFormData({ ...formData, title: e.target.value })} />
 
-                        <Select> 
+                        <Select 
+                        onChange={(e) => setFormData({ ...formData, category: e.target.value })}> 
                             <option value="uncategorized">Select a category</option>
                             <option value="general">General</option>
                             <option value="general">Scheduling</option>
@@ -89,7 +120,7 @@ const CreatePost = () => {
                             <Button 
                                 type ='button' 
                                 size='sm' 
-                                className="text-lg  bg-gradient-to-r from-green-500 to-blue-400 text-white px-14 py-4 rounded-md"
+                                className="text-lg  outline-black text-black px-14 py-4 rounded-md"
                                 
                                 onClick={handleUploadImage}
                                 disabled={imageUploadProgress}
@@ -113,10 +144,12 @@ const CreatePost = () => {
                         theme="snow"  
                         placeholder="Write something..." 
                         className="h-72 mb-12"
-                        required/> 
+                        required
+                        onChange={(value) => setFormData({ ...formData, content: value})}/> 
                     
                     <Button type="submit" className="text-3xl bg-green-500 text-white hover:bg-green-600 py-2 rounded-md">
                     Submit </Button>
+                    {publishError && <Alert className="mt-5" color="failure">{publishError}</Alert>}
                 </form>
                 
 
